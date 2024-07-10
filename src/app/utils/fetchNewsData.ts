ab13788850860be26e68e3d53fbdd31b78b47b6d
udayDatar <uday.datar@adglobal360.com>
2024-07-10 11:21:23 +0530

    modified news search parameters

--- src/app/utils/fetchNewsData.ts
+++ src/app/utils/fetchNewsData.ts
@@ -2,7 +2,7 @@
 
 export const fetchNewsData = async (stockSymbol: string) => {
     const apiKey = 'LxfOFGHySf3AAhfsdIN4t4cXZ4efT5ErOwZbPrnL'; // Replace with your actual API key for thenewsapi.com
-    const url = `https://api.thenewsapi.com/v1/news/all?api_token=${apiKey}&search=${stockSymbol}&language=en&categories=business`; // Adjusted query to include 'stock' for thenewsapi.com
+    const url = `https://api.thenewsapi.com/v1/news/all?api_token=${apiKey}&search=stock_symbol:${stockSymbol}&language=en&categories=business`;
 
     try {
         const response = await fetch(url);
